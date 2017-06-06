fa34553e78cdc35e8e554677b2f4cec184fcd8c6
1alanafs1-PC\1alanafs1 <alunnatic@yahoo.com>
2017-06-06 13:07:55 -0400

    github

---
+++ Assets/Scripts/Player/Stats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+    public float health;
+    public float armor;
+    public float playerSpeed;
+    public float fireRate;
+    public float bulletSpeed;
+    public float bulletPower;
+
+
+	// Use this for initialization
+	void Start () {
+    health = 10;
+    armor = 1;
+    playerSpeed = 5;
+    fireRate = .2f;
+    bulletSpeed = 10;
+    bulletPower = 5;
+
+}
+
+// Update is called once per frame
+void Update () {
+        if (health < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
